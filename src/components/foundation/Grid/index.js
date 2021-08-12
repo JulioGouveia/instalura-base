d8fb52ce73f58196d65b501adbe33f26d97fd868
unknown <juled>
2021-08-12 15:52:01 -0300

    implementação dos grids via propToStyle

--- src/components/foundation/Grid/index.js
+++ src/components/foundation/Grid/index.js
@@ -8,26 +8,27 @@ const Container = styled.div`
     padding-left: 28px;
     margin-right: auto;
     margin-left: auto;
+
     ${breakpointsMedia({
     xs: css`
-    max-width: initial;
+        max-width: initial;
 
-    padding-right: 28px;
-    padding-left: 28px;
+        padding-right: 28px;
+        padding-left: 28px;
     `,
     sm: css`
-    max-width: 576px; 
+        max-width: 576px; 
     `,
     md: css`
-    max-width: 768px;
-    padding-right: 16px;
-    padding-left: 16px; 
+        max-width: 768px;
+        padding-right: 16px;
+        padding-left: 16px; 
     `,
     lg: css`
-    max-width: 1160px; 
+        max-width: 1160px; 
     `,
     xl: css`
-    max-width: 1222px;
+        max-width: 1222px;
     `,
     })}
 `
