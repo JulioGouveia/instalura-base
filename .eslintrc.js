b67855a546cba13b1fb2ad628a91e87db955f3e4
unknown <juled>
2021-08-17 01:55:49 -0300

    adcição da lib motion da pages/index

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'eslint-disable import/prefer-default-export': 'off',
+    'eslint-disable linebreak-style' : 'on',
   },
 };
